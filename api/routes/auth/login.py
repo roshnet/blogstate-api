e01c41663dcd56a8e25f7b7c3660ea682881eb72
Roshan Sharma <reprogram46@gmail.com>
2019-11-01 00:20:03 +0530

    feat: Implement basic login/auth

--- api/routes/auth/login.py
+++ api/routes/auth/login.py
@@ -1,9 +1,31 @@
+import json
 from api import app
+
+# Models for database
+from api.models import Credentials
+
+# Password hash validation
+from werkzeug.security import (
+    check_password_hash as cph,
+    generate_password_hash as gph
+)
 
 
 class LoginResource:
     def on_post(self, req, resp):
-        resp.media = f"/login"
+        """
+        Expected params as body of POST request:
+            :param `username`
+            :param `passwd` 
+        """
+
+        creds = json.loads(req.stream.read())
+        record = Credentials.get(Credentials.username == creds['username'])
+
+        if cph(record.hash, gph(creds['passwd'])):
+            resp.body = json.dumps({"status": "verified"})
+        else:
+            resp.body = json.dumps({"status": "unauthorised"})
 
 
 app.add_route('/login', LoginResource())
