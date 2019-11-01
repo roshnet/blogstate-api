1f679379b33775cc5db2eda6c2dd4d40b4915e16
Roshan Sharma <reprogram46@gmail.com>
2019-11-01 13:07:08 +0530

    feat: Add user signup logic at /signup

--- api/routes/auth/signup.py
+++ api/routes/auth/signup.py
@@ -1,9 +1,42 @@
 from api import app
+import json
+
+# Models for database
+from api.models import Credentials as Creds
+
+# Password hash generation
+from werkzeug.security import generate_password_hash as gph
 
 
 class SignupResource:
     def on_post(self, req, resp):
-        resp.media = f"/signup"
+        """
+        Expected params in POST body:
+            :param `username`
+            :param `passwd`
+            :param `email`
+            :param `name`
+        """
+        _info = json.loads(req.stream.read())
+
+        # TODO: Test structure of incoming request body/params.
+
+        # Check username availability
+        if Creds.select().where(
+          Creds.username == _info['username']).exists():
+            resp.body = json.dumps({
+                "status": "fail",
+                "msg": "Username not available"
+            })
+        else:
+            # Proceed with insertion
+            Creds.create(
+                username=_info['username'],
+                hash=gph(_info['passwd'], method='sha1'),
+                email=_info['email'],
+                name=_info['name']
+            )
+            resp.body = json.dumps({"status": "pass"})
 
 
 app.add_route('/signup', SignupResource())
