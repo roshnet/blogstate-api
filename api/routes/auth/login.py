1e417b2d5b200e8e665eb5c628e93937eead32cd
Roshan Sharma <reprogram46@gmail.com>
2019-11-01 17:38:04 +0530

    chore: Enhance username search in login by get_or_none()

--- api/routes/auth/login.py
+++ api/routes/auth/login.py
@@ -2,7 +2,7 @@ import json
 from api import app
 
 # Models for database
-from api.models import Credentials
+from api.models import Credentials as Creds
 
 # Password hash validation
 from werkzeug.security import check_password_hash as cph
@@ -18,25 +18,15 @@ class LoginResource:
 
         _creds = json.loads(req.stream.read())
 
-        if Credentials.select().where(
-          Credentials.username == _creds['username']).exists():
-            rec = Credentials.get(Credentials.username == _creds['username'])
-            if cph(rec.hash, _creds['passwd']):
+        record = Creds.get_or_none(Creds.username == _creds['username'])
+        if record:
+            if cph(record.hash, _creds['passwd']):
                 resp.body = json.dumps({"status": "pass"})
             else:
                 resp.body = json.dumps({
                     "status": "fail",
                     "msg": "Incorrect password"
                 })
-            """
-            Above approach is failsafe, but slow, since "searching for
-            username existence" and then "fetching hash for that username"
-            are two different ops, and take alomst double the time.
-
-            A better approach is to search and fetch the hash for the
-            username in a single query.
-            It would double speed and also reduce load on the server.
-            """
         else:
             resp.body = json.dumps({
                 "status": "fail",
