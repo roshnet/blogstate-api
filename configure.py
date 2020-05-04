0c73d4f06616311e60ac90423f98b8e692c82723
Roshan Sharma <reprogram46@gmail.com>
2020-05-04 15:57:24 +0530

    configure.py: Split into dedicated functions
    Create and call dedicated fuctions for each task. Adding more tasks becomes easier that way.

--- configure.py
+++ configure.py
@@ -16,45 +16,15 @@ config['DEFAULT'] = {
 }
 
 
-def generate_config_file():
-    pass
-
-
-def main():
+def verify_credentials_and_setup_tables():
     """
-    Generates config file, creates tables.
+    Attempts connecting to the database based on supplied credentials.
+    Creates required tables if it can, else returns an error.
     """
-
-    print(
-        '================================'
-        '+++ DEVELOPMENT SERVER SETUP +++'
-        '================================'
-    )
-    sleep(1)
-
-    config['DEFAULT']['HOST'] = input(
-        'Server name/IP (leave blank if on local server): '
-    ) or config['DEFAULT']['HOST']
-    config['DEFAULT']['DATABASE'] = input(
-        'The name of the database you created '
-        '(leave blank if using `blogstate`): '
-    ) or config['DEFAULT']['DATABASE']
-    config['DEFAULT']['USER'] = input(
-        'User name for the database (leave blank if `root`): '
-    ) or config['DEFAULT']['USER']
-    config['DEFAULT']['PASSWORD'] = getpass(
-        'Password of development database (will be stored in the generated '
-        '"config.ini"): '
-    ) or config['DEFAULT']['PASSWORD']
-
-    print('\nAttempt connection to database and create tables ...')
-    sleep(0.5)
-
     db = pw.MySQLDatabase(config['DEFAULT']['database'],
                           user=config['DEFAULT']['user'],
                           password=config['DEFAULT']['password'],
                           host=config['DEFAULT']['host'])
-
     try:
         db.connect()
         db.create_tables([
@@ -62,20 +32,86 @@ def main():
             Posts,
         ])
     except pw.OperationalError:
+        return True
+    return None
+
+
+def set_interactive_credentials():
+    """
+    Obtain database from user to store in config file.
+    """
+
+    config['DEFAULT']['HOST'] = input(
+        'Server name/IP (leave blank if on local server): '
+    ) or config['DEFAULT']['HOST']
+
+    config['DEFAULT']['DATABASE'] = input(
+        'The name of the database you created (leave blank if using the '
+        'name "blogstate"): '
+    ) or config['DEFAULT']['DATABASE']
+
+    config['DEFAULT']['USER'] = input(
+        'User name for the database (leave blank if `root`): '
+    ) or config['DEFAULT']['USER']
+
+    config['DEFAULT']['PASSWORD'] = getpass(
+        'Password of development database (will be stored in the generated '
+        '"config.ini"): '
+    ) or config['DEFAULT']['PASSWORD']
+
+
+def generate_config_file():
+    """
+    Generates an ini file of the configuration which works so far.
+    Running this function overwrites any previous setup values.
+    """
+
+    config['TOKENS'] = {}
+    config['TOKENS']['API'] = 'secret'
+    # This token is to be set in the `Authorization` header in all requests to
+    # the API. For development purposes, it can be set to something easy to
+    # remember.
+
+    with open('config', 'w') as cfg:
+        config.write(cfg)
+
+
+def main():
+    """
+    Set local machine up for API development.
+    Generates config file, creates tables, creates virtual environment.
+    """
+
+
+if __name__ == "__main__":
+    print(
+        '================================'
+        '+++ DEVELOPMENT SERVER SETUP +++'
+        '================================'
+    )
+    sleep(1)
+
+    # A series of inputs to set database connection variables
+    set_interactive_credentials()
+
+    print('\nAttempt connection to database and create tables ...')
+    sleep(0.5)
+
+    err = verify_credentials_and_setup_tables()
+    if err is not None:
         print(
-            '\n[EXIT] An error occurred. This is an issue with either the '
+            '[EXIT] An error occurred. This is an issue with either the '
             'values you entered, or with your MySQL server installation.'
         )
         exit(1)
+
+    # Freeze the values to a "config.ini" file
+    generate_config_file()
 
     # Finish setup, database looks good, all set!
     sleep(0.5)
     print(
         'The database has been set up. You can create users, add posts '
-        'and play around with the application.\n'
+        'and play around with the API.\n'
         'Please run this setup again if you supplied incorrect values.'
     )
-
-
-if __name__ == "__main__":
-    main()
